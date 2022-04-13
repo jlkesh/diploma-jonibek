52a759e917363ac2655f4ee89fde65bf9f22c15d
jlkesh <john.lgd65@gmail.com>
2022-04-13 11:24:32 +0500

    change for

--- app/schema/localization.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from pydantic import BaseModel
-from fastapi_localization import TranslateJsonResponse
-from fastapi_localization import TranslatableStringField
-
-
-class LanguageTranslatableSchema(BaseModel):
-    code: str
-    title: TranslatableStringField
