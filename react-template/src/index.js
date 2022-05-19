11fee6ab2411e0c248381ab70a147fea4abf4f98
Elmurodov Javohir <john.lgd65@gmail.com>
2022-05-19 13:20:20 +0500

    ui deleted

--- react-template/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./App";
-import "bulma/css/bulma.min.css";
-import {UserProvider} from "./context/UserContext";
-
-ReactDOM.render(<UserProvider><App/></UserProvider>, document.getElementById('root'));
